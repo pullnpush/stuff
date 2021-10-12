ae4fffa092f818a99005232af19a2b7546534d23
Andrey <sendhere0795@gmail.com>
2021-10-12 20:10:59 +0300

    begin

---
+++ src/stuff/Main.java
@@ -0,0 +1,20 @@
+package stuff;
+
+import sun.jvm.hotspot.oops.Array;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int [] originmas = new int [] {0,5,10,0,11,14,13,4,11,8,8,7,1,4,12,11};
+        int [] mas = new int [originmas.length];
+        int max=0;
+        int step=0;
+
+        do {
+
+        }
+        while (Arrays.equals(originmas,mas));
+    }
+}
